b478a7d9be62693e4c2949f2d7a19cf67ea39ef2
Rommel <925454349@qq.com>
2026-06-02 15:21:58 +0800

    feat: 添加 hooks.json 和 session-start 脚本模板

---
+++ bin/superspec.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/**
+ * superSpec CLI 入口
+ *
+ * 用法：
+ *   superspec init          初始化项目骨架
+ *   superspec validate <n>  校验 spec 文件
+ *   superspec --help        显示帮助
+ *   superspec --version     显示版本
+ */
+
+import '../dist/cli/index.js';
